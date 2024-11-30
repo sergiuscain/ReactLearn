77b10cadec176671b67eeed243c431706492cce4
sergiuscain <sergiussmithe@gmail.com>
2024-11-30 18:02:02 +0300

    Подключил Bootstrap, попробовал использовать его

--- src/index.js
+++ src/index.js
@@ -4,27 +4,23 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const elements = ["Lorem ipsum dolor sit amet.","Laboriosam dolorum ex culpa cupiditate.","Veniam, assumenda explicabo! Dolorum, obcaecati","Nobis, in nulla. Repellat, ea.","Dolores tempora placeat vel expedita!"];
 const firstName = "Сергей";
 const lastName = "Королев";
 const currentDate = new Date();
+const collection = [];
 const getRandom = () => {
   return Math.floor(Math.random()*10);
 }
+for(let i = 10; i<100;i++){
+  collection.push(i)
+};
 root.render(
   <div>
     <h1>Какой-то текст</h1>
 
     <h2>{firstName} {lastName.substring(0,3)}.</h2>
     <ul>
-      {
-        elements.map(x => (<li>{x}</li>))
-      }
-      {
-        Array.from({length: 5}, (_, index) =>
-          <li>{`Элемент ${getRandom()}`}</li>
-        )
-      }
+      {collection.map(x => (x%10==0 ? <br/> : <button className='btn btn-primary'>{x}</button>))}
     </ul><br/>
     <p>&copy; Авторские права мои {currentDate.getFullYear()}</p>
   </div>
