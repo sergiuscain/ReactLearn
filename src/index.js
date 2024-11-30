ab3793836be69f6eaf831436b7c7aef6077fde39
sergiuscain <sergiussmithe@gmail.com>
2024-11-30 17:16:08 +0300

    Вместо циклов

--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ const elements = ["Lorem ipsum dolor sit amet.","Laboriosam dolorum ex culpa cup
 const firstName = "Сергей";
 const lastName = "Королев";
 const currentDate = new Date();
+const getRandom = () => {
+  return Math.floor(Math.random()*10);
+}
 root.render(
   <div>
     <h1>Какой-то текст</h1>
 
     <h2>{firstName} {lastName.substring(0,3)}.</h2>
     <ul>
-      <li>{elements[4]}</li>
-      <li>{elements[3]}</li>
-      <li>{elements[2]}</li>
-      <li>{elements[1]}</li>
-      <li>{elements[0]}</li>
+      {
+        elements.map(x => (<li>{x}</li>))
+      }
+      {
+        Array.from({length: 5}, (_, index) =>
+          <li>{`Элемент ${getRandom()}`}</li>
+        )
+      }
     </ul><br/>
     <p>&copy; Авторские права мои {currentDate.getFullYear()}</p>
   </div>
