ea8eead42e9e8d834ae3c2bf43520c75f145e38b
sergiuscain <sergiussmithe@gmail.com>
2024-12-08 21:32:04 +0300

    Ничего не работает, ничего не понимаю....с моего api данные не подтягиваются почему-то:(

--- src/Layout/Home/HomePage.jsx
+++ src/Layout/Home/HomePage.jsx
@@ -4,7 +4,8 @@ import TopMenu from "../General/NavMenu";
 import Content from "./Content";
 import Footer from "../General/Footer";
 
-const url = "https://localhost:7210/api/Notes/GetById?id=22c98a74-2913-4284-8b9c-b3e528838ba1";
+const baseApiUrl = process.env.BASE_API_URL;
+
 const AddNote = (titleText, descriptionText) =>{
     const note = {
         userId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
@@ -13,55 +14,17 @@ const AddNote = (titleText, descriptionText) =>{
     };
     alert(`title=${titleText} description=${descriptionText}`);
 };
-const notes = [
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "descriptLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationion1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "descriptiLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationon1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "descLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationription1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "desLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationcription1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "description1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "descripLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationtion1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "description1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "descripLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationtion1"
-    },
-    {
-        "id":1,
-        "title": "Title1",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam rationLorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus totam ullam ratione, error voluptas doloremque! Aut magni excepturi amet ad ea dicta est quas, expedita inventore delectus, ab quibusdam."
-    },
-]
+
 
 const HomePage = () => {
+
+    const [notes, setNotes] = useState([])
+    const urlGet = `${baseApiUrl}/getbyid`;
+    useEffect(() => {
+        axios.get(urlGet).then(
+            res => setNotes.log(res.data)
+        );
+    })
 
     return(
         <div>
